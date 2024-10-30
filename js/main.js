9a84674641871b166717f27897a7de1e9e3d0845
Sulekh25 <sulekhniwal@gmail.com>
2024-10-30 11:17:50 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -113,7 +113,7 @@
     
 
     // Use this for real timer date
-    var timerdate = "2023/11/24";
+    var timerdate = "2024/11/27";
 
 	$("#countdown").countdown(timerdate, function(event) {
         $(this).html(event.strftime("<div class='cd-item'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-item'><span>%H</span> <p>Hrs</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Mins</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Secs</p> </div>"));
